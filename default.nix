4fd09dbd1a374aecefc6097c60837680184a63cd
Heph <srht@mrkeebs.eu>
2021-10-31 14:56:02 +0100

    Adding Nfstream python lib

--- default.nix
+++ default.nix
@@ -14,6 +14,10 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
+  ### DEVELOPMENT
+  nfstream = pkgs.callPackage ./pkgs/nfstream { };
+
+  ### APPLICATION
   atlas = pkgs.callPackage ./pkgs/atlas { };
   logisim-evolution = pkgs.callPackage ./pkgs/logisim-evolution { };
 }
